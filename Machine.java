44e1837d803f9bbbedc4df079ab704926f0514fe
CraigeHales <71161286+CraigeHales@users.noreply.github.com>
2025-06-11 14:09:57 -0400

    Update Machine.java

--- Machine.java
+++ Machine.java
@@ -1,2 +1,13 @@
-test
-q
+package controller; // the (virtual) pathname to the student-written files
+import controller.Executer; // The interface the hardware expects this class to implement
+import controller.PostResult; // The callback the hardware expects this class to use
+import controller.Aa; // other student-written
+import controller.Bb; // classes that are needed
+import controller.Cc; // by the Machine class
+public class Machine implements Executer {
+  public void doClick(PostResult result, String id){
+    // process the click from the svg "id"
+    Aa.dosomething();
+    result.setText("some_id","some_text");
+  }
+}
