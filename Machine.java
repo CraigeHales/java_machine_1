f18913c54523e3b3da503807810df4752ce5ddd8
CraigeHales <71161286+CraigeHales@users.noreply.github.com>
2025-06-12 12:21:10 -0400

    Update Machine.java

--- Machine.java
+++ Machine.java
@@ -4,10 +4,11 @@ import controller.PostResult; // The callback the hardware expects this class to
 // import controller.Aa; // other student-written
 // import controller.Bb; // classes that are needed
 // import controller.Cc; // by the Machine class
+import Buttons;
 public class Machine implements Executer {
     public void doClick(PostResult result, String id){
         // process the click from the svg "id"
-//        Aa.dosomething();
+        Buttons.hello(result);
         result.setText("some_id","some_text");
     }
 }
