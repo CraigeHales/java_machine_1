f97b7cda8b5a8b3e029d8611cec56f74796c1afb
CraigeHales <71161286+CraigeHales@users.noreply.github.com>
2025-06-16 14:21:46 -0400

    Update Buttons.java

--- Buttons.java
+++ Buttons.java
@@ -3,6 +3,6 @@ import controller.PostResult;
 public class Buttons {
     static public void hello(PostResult result){
       result.setText("some_other_id","some_other_text");
-      System.out.println("hi from Bb.hello12");
+      System.out.println("hi from Bb.hello124");
     }
 }
