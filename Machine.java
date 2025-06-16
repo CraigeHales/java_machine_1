092764b070538d2176729f1d386bdf6b5ace6ff3
CraigeHales <71161286+CraigeHales@users.noreply.github.com>
2025-06-16 17:06:27 -0400

    Update Machine.java

--- Machine.java
+++ Machine.java
@@ -7,8 +7,15 @@ import controller.PostResult; // The callback the hardware expects this class to
 import controller.Buttons;
 public class Machine implements Executer {
     public void doClick(PostResult result, String id){
-        System.out.println("machine running");
-        Buttons.hello(result);
+        System.out.println("machine running 2");
+        try {
+            Buttons.hello(result);
+        }
+        catch (Exception e){
+            System.out.println(e);
+            System.out.println(e.printStackTrace());
+        }
+            
         result.setText("some_id","some_texttttt");
     }
 }
