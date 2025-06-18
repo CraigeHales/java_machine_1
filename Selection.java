8b621ca5824e185e9c54e85a16223ebb03acbbba
CraigeHales <71161286+CraigeHales@users.noreply.github.com>
2025-06-18 12:01:32 -0400

    Create Selection.java

---
+++ Selection.java
@@ -0,0 +1,7 @@
+package controller;
+import controller.PostResult;
+public class Selection {
+    public void press(String id, PostResult result){
+      result.setText("some_other_id","some_other_text");
+    }
+}
