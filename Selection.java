06c272eb1fba68c407c88a095595c79e38f55fe9
CraigeHales <71161286+CraigeHales@users.noreply.github.com>
2025-06-20 23:48:22 -0400

    Update Selection.java

--- Selection.java
+++ Selection.java
@@ -2,7 +2,7 @@ package controller;
 import controller.PostResult;
 public class Selection {
     public void press(String id, PostResult result){
-      result.setText("some_other_id","some_other_textx");
+      result.set f Text("some_other_id","some_other_textx");
         
     }
 }
