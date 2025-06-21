a112ebdd6d746711c7cd61a20ae61be64534b805
CraigeHales <71161286+CraigeHales@users.noreply.github.com>
2025-06-21 11:47:56 -0400

    Update Selection.java

--- Selection.java
+++ Selection.java
@@ -2,12 +2,14 @@ package controller;
 import controller.PostResult;
 public class Selection {
     public void press(String id, PostResult result){
-      result.setText("some_other_id","some_other_textx");
-        result.println("---------before---------");
-        String x=null;
-        System.out.println("aaa"+x.toString());
-        x=x+1;
-        System.out.println("bbb"+x.toString());
-        result.println("---------after---------");
+        result.setText("some_other_id","some_other_textx");
+        if (id.startsWith("rect")) {
+            result.println("---------before---------");
+            String x=null;
+            System.out.println("aaa"+x.toString());
+            x=x+1;
+            System.out.println("bbb"+x.toString());
+            result.println("---------after---------");
+        }
     }
 }
