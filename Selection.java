33317334a24991562efea11a8d403f7a4d3d9162
CraigeHales <71161286+CraigeHales@users.noreply.github.com>
2025-06-21 07:10:21 -0400

    Update Selection.java

--- Selection.java
+++ Selection.java
@@ -4,7 +4,7 @@ public class Selection {
     public void press(String id, PostResult result){
       result.setText("some_other_id","some_other_textx");
         result.println("---------before---------");
-        String x=null;result.println(x);
+        String x=null;System.out.println(x);
         result.println("---------after---------");
     }
 }
