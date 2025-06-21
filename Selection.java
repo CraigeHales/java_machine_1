4613694a79390f971c9c4a8ee8c34fe3de15a6d3
CraigeHales <71161286+CraigeHales@users.noreply.github.com>
2025-06-21 00:27:02 -0400

    Update Selection.java

--- Selection.java
+++ Selection.java
@@ -3,8 +3,8 @@ import controller.PostResult;
 public class Selection {
     public void press(String id, PostResult result){
       result.setText("some_other_id","some_other_textx");
-        System.out.println("---------before---------");
-        String x=null;x=x.substring(88,888);
-        System.out.println("---------after---------");
+        result.println("---------before---------");
+        String x=null;result.println(x);
+        result.println("---------after---------");
     }
 }
