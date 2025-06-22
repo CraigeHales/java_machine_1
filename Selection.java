beafa7e6c5d5da775e3a555926a33049c400686b
CraigeHales <71161286+CraigeHales@users.noreply.github.com>
2025-06-22 11:46:38 -0400

    Update Selection.java

--- Selection.java
+++ Selection.java
@@ -24,14 +24,8 @@ public class Selection {
     }
     
     public void press(String id, PostResult result){
-        result.setText("some_other_id","some_other_textx");
-        if (id.startsWith("rect")) {
-            result.println("---------before---------");
-            String x=null;
-            System.out.println("aaa"+x.toString());
-            x=x+1;
-            System.out.println("bbb"+x.toString());
-            result.println("---------after---------");
-        }
+        static boolean x = false;
+        x = !x;
+        result.setColor("circle_github_load",x ? "red" : "blue");
     }
 }
