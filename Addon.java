a9a7af0c849eb3723cf7a2162cfb13a6408a13b7
CraigeHales <71161286+CraigeHales@users.noreply.github.com>
2025-06-22 19:03:23 -0400

    z

--- Addon.java
+++ Addon.java
@@ -4,12 +4,12 @@ public class Addon {
     String name;
     int pricePennies;
 
-    Public Addon(String name, int pricePennies) {
+    public Addon(String name, int pricePennies) {
         this.name = name;
         this.pricePennies = pricePennies;
     }
 
-    static void reset(PostResult result){
+    public static void reset(PostResult result){
         result.setText("tspan_add1","Pick a");
         result.setText("tspan_add2","Drink");
         result.setText("tspan_add3","Above");
