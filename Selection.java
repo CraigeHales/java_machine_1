a4bc9b51d75c57cff4d29adee5968b27df220526
CraigeHales <71161286+CraigeHales@users.noreply.github.com>
2025-06-21 21:47:40 -0400

    Update Selection.java

--- Selection.java
+++ Selection.java
@@ -1,6 +1,27 @@
 package controller;
 import controller.PostResult;
 public class Selection {
+    String prodName;
+    String id;
+    String textColor;
+    String backColor;
+    Addon add[3];
+    int prodPennies;
+    int nStock;
+    public Selection(String prodName,String id,String textColor,String backColor,
+        int prodPennies,int nStock,Addon add0,Addon add1,Addon add2){
+    this.prodName = prodName;
+    this.id = id;
+    this.textColor = textColor;
+    this.backColor = backColor;
+    this.add = new Addon()[3];
+    this.add[0] = add0; 
+    this.add[1] = add1; 
+    this.add[2] = add2; 
+    this.prodPennies = prodPennies;
+    this.nStock = nStock;
+        
+    }
     public void press(String id, PostResult result){
         result.setText("some_other_id","some_other_textx");
         if (id.startsWith("rect")) {
