4a0e430e553a79b565f83a2b9ff8c97a82a73ab3
CraigeHales <71161286+CraigeHales@users.noreply.github.com>
2025-06-22 07:59:23 -0400

    Update Selection.java

--- Selection.java
+++ Selection.java
@@ -8,20 +8,21 @@ public class Selection {
     Addon[] add;
     int prodPennies;
     int nStock;
+    
     public Selection(String prodName,String id,String textColor,String backColor,
         int prodPennies,int nStock,Addon add0,Addon add1,Addon add2){
-    this.prodName = prodName;
-    this.id = id;
-    this.textColor = textColor;
-    this.backColor = backColor;
-    this.add = new Addon[3];
-    this.add[0] = add0; 
-    this.add[1] = add1; 
-    this.add[2] = add2; 
-    this.prodPennies = prodPennies;
-    this.nStock = nStock;
-        
+        this.prodName = prodName;
+        this.id = id;
+        this.textColor = textColor;
+        this.backColor = backColor;
+        this.add = new Addon[3];
+        this.add[0] = add0; 
+        this.add[1] = add1; 
+        this.add[2] = add2; 
+        this.prodPennies = prodPennies;
+        this.nStock = nStock;    
     }
+    
     public void press(String id, PostResult result){
         result.setText("some_other_id","some_other_textx");
         if (id.startsWith("rect")) {
