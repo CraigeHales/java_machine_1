0aaac5ebe92f8a66c1e9b426e8d3823dd7929313
CraigeHales <71161286+CraigeHales@users.noreply.github.com>
2025-06-23 09:11:46 -0400

    c

--- Selection.java
+++ Selection.java
@@ -31,6 +31,7 @@ public class Selection {
     public void press(PostResult result){
         for(int i=0; i<add.length; i+=1) {
             add[i].activateButton(result, "tspan_add"+i);
+            result.println("select "+prodName)
         }
     }
 }
