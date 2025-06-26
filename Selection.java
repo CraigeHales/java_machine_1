34dbe05a2994002fe7d9b5d9514c7070eece1639
CraigeHales <71161286+CraigeHales@users.noreply.github.com>
2025-06-26 10:57:52 -0400

    x

--- Selection.java
+++ Selection.java
@@ -35,21 +35,22 @@ public class Selection {
     }
     
     String dim(String c) { // cut the brightness in half. more or less.
-        c.replace("1","0");
-        c.replace("2","1");
-        c.replace("3","1");
-        c.replace("4","2");
-        c.replace("5","2");
-        c.replace("6","3");
-        c.replace("7","3");
-        c.replace("8","4");
-        c.replace("9","4");
-        c.replace("a","5");
-        c.replace("b","5");
-        c.replace("c","6");
-        c.replace("d","6");
-        c.replace("e","7");
-        c.replace("f","7");
+        c=c.replace("1","0");
+        c=c.replace("2","1");
+        c=c.replace("3","1");
+        c=c.replace("4","2");
+        c=c.replace("5","2");
+        c=c.replace("6","3");
+        c=c.replace("7","3");
+        c=c.replace("8","4");
+        c=c.replace("9","4");
+        c=c.replace("a","5");
+        c=c.replace("b","5");
+        c=c.replace("c","6");
+        c=c.replace("d","6");
+        c=c.replace("e","7");
+        c=c.replace("f","7");
+        return c;
     }
     public void off(PostResult result){
         result.setColor("rect_"+id, dim(backColor));
