10d4506c17287feaf949cf3f53f2f9106570c68d
CraigeHales <71161286+CraigeHales@users.noreply.github.com>
2025-06-26 11:10:28 -0400

    x

--- Buttons.java
+++
@@ -1,8 +0,0 @@
-package controller;
-import controller.PostResult;
-public class Buttons {
-    static public void hello(PostResult result){
-      result.setText("some_other_id","some_other_text");
-      //System.out.println("hi from Bb.hello1245678");
-    }
-}
