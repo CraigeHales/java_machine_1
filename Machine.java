49f3181c79e28d2b760d8aaf1f2e295680ea3b25
CraigeHales <71161286+CraigeHales@users.noreply.github.com>
2025-07-06 15:27:39 -0400

    h

--- Machine.java
+++ Machine.java
@@ -175,7 +175,7 @@ public class Machine implements Executer {
             }
 
             result.setTransform("idLiquidDrinkTransform", "matrix(1,0,0,1,0,0.0)", delay);
-            delay += 400;
+            delay += 2000;
 
             if ( gCurrentSelection.has("Vanilla") ) {
                 result.setTransform("idVanillacreamAddinTransform", "matrix(1,0,0,1,0,0.0)", delay);
